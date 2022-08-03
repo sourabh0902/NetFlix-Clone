58d855793164629fa8302606725e68363c61ea6a
sourabh0902 <sg456190@gmail.com>
2022-08-03 23:26:30 +0530

    Netflix-Clone commit

---
+++ public/js/api.js
@@ -0,0 +1,8 @@
+let api_key = "fc4695d55613b2224eda5d0da94de590";
+
+let img_url = "https://image.tmdb.org/t/p/w500";
+let original_img_url = "https://image.tmdb.org/t/p/original";
+
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?";
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?";
+let movie_detail_http = "https://api.themoviedb.org/3/movie";
